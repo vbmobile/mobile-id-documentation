84eaf95c32858c3f8efff891042978cc775621b7
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-07-23 15:44:17 +0100

    TRAVELERID-15227: Update iOS docs for 9.2.1 and AMAShareUltralight 2.0.13

--- docs_macros.py
+++ docs_macros.py
@@ -3,11 +3,11 @@ VERSIONS = {
     "android_doc_scan_mrz_provider": "2.0.1",
     "android_doc_rfid_read_provider": "2.0.1",
     "android_ultralight_provider": "2.0.3",
-    "ios_enrolment_sdk": "9.2.0",
+    "ios_enrolment_sdk": "9.2.1",
     "ios_doc_scan_regula_provider": "2.0.2",
     "ios_doc_scan_mrz_provider": "2.0.3",
     "ios_doc_rfid_read_provider": "2.0.2",
-    "ios_ultralight_provider": "2.0.4",
+    "ios_ultralight_provider": "2.0.13",
 }
 
 def define_env(env):
